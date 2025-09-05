339ac9ab76d464405d2e3cd36256a77ddb53f6db
Ankush Kumar <82904908+Ankushkr13@users.noreply.github.com>
2025-09-06 00:05:19 +0530

    design changes

--- expense-tracker-frontend/src/index.js
+++ expense-tracker-frontend/src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { UserProvider } from './contexts/UserContext'; 
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
